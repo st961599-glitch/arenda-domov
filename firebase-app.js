93468ab2799f06de13265698f3c8fed536e545ca
st961599-glitch <st961599@ruc.su>
2026-06-17 13:15:09 +0300

    Update firebase-app.js

--- firebase-app.js
+++ firebase-app.js
@@ -1,4 +1,20 @@
 export * from "https://gstatic.com";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDm3XvtldDnpLDH29uV8e0722nWj08vNqA",
+  authDomain: "arenda-domov-e2e15.firebaseapp.com",
+  projectId: "arenda-domov-e2e15",
+  storageBucket: "arenda-domov-e2e15.firebasestorage.app",
+  messagingSenderId: "297684238985",
+  appId: "1:297684238985:web:727fd972cdc8465a55fc58",
+  measurementId: "G-6GZ904F8D1"
+};
+  
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
+
 // Регистрация
 function register() {
     const email = document.getElementById('registerEmail').value;
