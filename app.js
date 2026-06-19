ae78997c1ca97b58684cb1cc37ab2c80750197c9
st961599-glitch <st961599@ruc.su>
2026-06-19 10:41:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,71 +7,7 @@ const firebaseConfig = {
   appId: "1:297684238985:web:727fd972cdc8465a55fc58",
   measurementId: "G-6GZ904F8D1"
 };
-// Регистрация
-function register() {
-    const emailInput = document.getElementById('registerEmail');
-    const passwordInput = document.getElementById('registerPassword');
-    const nameInput = document.getElementById('registerName');
 
-    if (!emailInput || !passwordInput || !nameInput) {
-        alert('Ошибка: Не удалось найти поля ввода на странице!');
-        return;
-    }
-
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-    const name = nameInput.value.trim();
-
-    if (!email || !password || !name) {
-        alert('Пожалуйста, заполните все поля для регистрации!');
-        return;
-    }
-
-    auth.createUserWithEmailAndPassword(email, password)
-        .then(credential => {
-            return credential.user.updateProfile({ displayName: name });
-        })
-        .then(() => {
-            alert('Регистрация прошла успешно!');
-            showPage('catalogPage');
-            switchTab('productsTab');
-            loadProducts();
-        })
-        .catch(error => alert('Ошибка регистрации: ' + error.message));
-}
-
-// Вход
-function login() {
-    const emailInput = document.getElementById('loginEmail');
-    const passwordInput = document.getElementById('loginPassword');
-
-    if (!emailInput || !passwordInput) {
-        alert('Ошибка: Не удалось найти поля ввода для входа!');
-        return;
-    }
-
-    const email = emailInput.value.trim();
-    const password = passwordInput.value.trim();
-
-    if (!email || !password) {
-        alert('Пожалуйста, введите Email и Пароль!');
-        return;
-    }
-
-    auth.signInWithEmailAndPassword(email, password)
-        .then(() => {
-            alert('Добро пожаловать!');
-            showPage('catalogPage');
-            switchTab('productsTab');
-            loadProducts();
-        })
-        .catch(error => alert('Ошибка входа: ' + error.message));
-}
-
-// Выйти
-function logout() {
-    auth.signOut();
-}
  function showLogin() {
     showPage('loginPage');
 }
