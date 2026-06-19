d7661a22ebbb62b7e9a8fd48039a09d0b842fce8
st961599-glitch <st961599@ruc.su>
2026-06-19 10:53:23 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,16 @@
-// Автоматическое переключение кнопок в шапке сайта
-onAuthStateChanged(auth, (user) => {
-    const btnLogin = document.getElementById('btnShowLogin');
-    const btnRegister = document.getElementById('btnShowRegister');
-    const btnLogout = document.getElementById('btnLogout');
-    const userEmail = document.getElementById('userEmail');
+const firebaseConfig = {
+  apiKey: "AIzaSyDm3XvtldDnpLDH29uV8e0722nWj08vNqA",
+  authDomain: "arenda-domov-e2e15.firebaseapp.com",
+  projectId: "arenda-domov-e2e15",
+  storageBucket: "arenda-domov-e2e15.firebasestorage.app",
+  messagingSenderId: "297684238985",
+  appId: "1:297684238985:web:727fd972cdc8465a55fc58",
+  measurementId: "G-6GZ904F8D1"
+};
 
-    if (user) {
-        // Если вошел: прячем Вход/Регистрацию, показываем Email и Выйти
-        if(btnLogin) btnLogin.style.display = 'none';
-        if(btnRegister) btnRegister.style.display = 'none';
-        if(btnLogout) btnLogout.style.display = 'inline-block';
-        if(userEmail) {
-            userEmail.style.display = 'inline-block';
-            userEmail.textContent = user.email;
-        }
-    } else {
-        // Если вышел: показываем Вход/Регистрацию, прячем Выйти
-        if(btnLogin) btnLogin.style.display = 'inline-block';
-        if(btnRegister) btnRegister.style.display = 'inline-block';
-        if(btnLogout) btnLogout.style.display = 'none';
-        if(userEmail) userEmail.style.display = 'none';
-    }
-});
-
+ function showLogin() {
+    showPage('loginPage');
+}
+function showRegister() {
+    showPage('registerPage');
+}
