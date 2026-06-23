6d43c5e8f27162a07300e113b0419ebbef35a896
st961599-glitch <st961599@ruc.su>
2026-06-23 14:14:06 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,13 +20,15 @@ import {
  *  ПОЛЕт “firebaseConfig” вставь целиком как объект.
  */
 const firebaseConfig = {
-  apiKey: "ВАШ_API_KEY",
-  authDomain: "ВАШ_AUTH_DOMAIN",
-  projectId: "ВАШ_PROJECT_ID",
-  storageBucket: "ВАШ_STORAGE_BUCKET",
-  messagingSenderId: "ВАШ_MESSAGING_SENDER_ID",
-  appId: "ВАШ_APP_ID"
+  apiKey: "AIzaSyDm3XvtldDnpLDH29uV8e0722nWj08vNqA",
+  authDomain: "arenda-domov-e2e15.firebaseapp.com",
+  projectId: "arenda-domov-e2e15",
+  storageBucket: "arenda-domov-e2e15.firebasestorage.app",
+  messagingSenderId: "297684238985",
+  appId: "1:297684238985:web:727fd972cdc8465a55fc58",
+  measurementId: "G-6GZ904F8D1"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
